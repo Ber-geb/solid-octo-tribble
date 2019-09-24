789119c4a9046ab54a51dfb4594596b7e190b7db
Ber-geb <gebremed@unlv.nevada.edu>
2019-09-24 01:36:39 -0700

    TIVAC Assignment 3

---
+++ TIVAC/LAB03/README.txt
@@ -0,0 +1,7 @@
+Name: Serak Gebremedhin
+Student NSHE: 2000862766
+Youtube Links : 
+https://youtu.be/tfFwKN7x-zY
+https://youtu.be/5gl26b-mw8s
+https://youtu.be/Ep0y8chsmyg
+https://youtu.be/Ow0215id8XE
---
+++ TIVAC/LAB03/task2a.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+
+uint8_t ui8PinData=4; //R = 2, G = 8, B = 4
+
+int main(void)
+{
+    SysCtlClockSet(SYSCTL_SYSDIV_15|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+    //delay = Delay time(seconds)/(1/SysClk*3) where SysClk = (400M/2)/15 and Delay time = 0.5 s
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+    //Selecting GPIO pins as output pins
+
+    while(1)
+    {
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1| GPIO_PIN_2| GPIO_PIN_3, ui8PinData);
+        SysCtlDelay(2222222.222); //calculated delay on time
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x00);
+        SysCtlDelay(2222222.222); // calculated delay off time
+        if(ui8PinData==4) {ui8PinData=8;} //BGR sequence
+        else if (ui8PinData==8) {ui8PinData=2;}
+        else {ui8PinData=4;}
+    }
+}
